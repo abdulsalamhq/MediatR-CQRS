fcc14f27b95af0ca9b46ecfe6f6807e06a402760
AbdulSalam <AbdulSalam@ABDULSALAM-PC>
2022-01-28 10:31:07 +0500

    Add project files.

---
+++ EmployeeManagementLibrary/Data/DataAccess.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeManagementLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementLibrary.Data
+{
+    public class DataAccess : IDataAccess
+    {
+        public List<EmployeeModel> employees = new();
+        public DataAccess()
+        {
+            employees.Add(new EmployeeModel { Id = 1, FirstName = "Abdul", LastName = "Salam" });
+            employees.Add(new EmployeeModel { Id = 2, FirstName = "Mohammad", LastName = "Abdullah" });
+        }
+
+        public List<EmployeeModel> GetEmployees()
+        {
+            return employees;
+        }
+        public EmployeeModel AddEmployees(string firstName, string lastName)
+        {
+            EmployeeModel employeeModel = new()
+            {
+                FirstName = firstName,
+                LastName = lastName
+
+            };
+            employeeModel.Id = employees.Max(x => x.Id) + 1;
+            return employeeModel;
+        }
+
+
+    }
+}
